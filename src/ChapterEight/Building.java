34a56da5bb778f8f8e825f8f6245cf565997f914
Ladipo Sanni <ladicorp24m@gmail.com>
2022-05-09 13:57:01 +0100

    Building Description

---
+++ src/ChapterEight/Building.java
@@ -0,0 +1,39 @@
+
+package ChapterEight;
+public class Building {
+
+
+    String name;
+    String type;
+    String address;
+    int numbersOfFloors;
+    int numbersOfRooms;
+
+    public Building() {
+        name = "Alhaji House";
+        type = "Residential";
+        address = "3, Oladeinde close, ojuelegba Lagos.";
+        numbersOfFloors = 2;
+        numbersOfRooms = 18;
+
+
+
+    }
+    public String getName(){ return name;}
+    public String getType(){return type;}
+    public String getAddress(){return address;}
+    public int getNumberOfFloors(){return numbersOfFloors;}
+    public int getNumbersOfRooms(){return numbersOfRooms;}
+
+    public static void main(String[] args) {
+        Building house = new Building();
+
+        System.out.printf("%s%n%s%n%s%n%d%n%d",house.getName(),house.getType(),house.getAddress(),
+                house.getNumberOfFloors(),house.getNumbersOfRooms());
+    }
+
+}
+
+
+
+
