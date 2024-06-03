1874a55ce87fc508e10298aa10c25a3517f0fed8
LadipoSanni <101804382+LadipoSanni@users.noreply.github.com>
2024-06-03 17:21:04 +0100

    Update TraineeRestController.java

--- TraineeRestController.java
+++ TraineeRestController.java
@@ -58,6 +58,4 @@ public class TraineeRestController {
     public ApiResponse viewLoanApplicationInformation (@PathVariable String traineeId) throws LearnSpaceException{
         return ApiResponse.ok(traineeController.viewLoanApplicationInfo(traineeId));
     }
-
-
 }
